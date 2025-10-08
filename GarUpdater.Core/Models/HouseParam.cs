0ce78ac86cd032f5065024049fa012aa0620e11b
Stepan Smorodnikov <positivepel@gmail.com>
2025-10-08 12:38:59 +0500

    [add] Добавлена модель, загружены структуры

---
+++ GarUpdater.Core/Models/HouseParam.cs
@@ -0,0 +1,15 @@
+﻿namespace GarUpdater.Core.Models
+{
+    public class HouseParam
+    {
+        public string Id { get; set; } = default!;
+        public string ObjectId { get; set; } = default!;
+        public string ChangeId { get; set; } = default!;
+        public string? ChangeIdEnd { get; set; }
+        public string TypeId { get; set; } = default!;
+        public string? Value { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
